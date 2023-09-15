5cab91e9396ad34a97f7944220e936ccb65e5305
Rutvij Patel <rutvij.patel@embifi.in>
2023-09-15 11:09:25 +0530

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { findOccurrences } from './utils/tracker'
+import { displayGraphWithCode, displayResults } from './utils/display'
+import { Graph } from 'graphlib'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const searchString = "searchString"; 
+  async function runAnalysis() {
+    const occurrences = await findOccurrences(searchString);
+    displayResults(occurrences);
+    
+    const graph = new Graph();
+    await displayGraphWithCode(graph, occurrences);
+    }
+  useEffect(() => {
+    runAnalysis();
+  }
+  , [ count]);
   return (
     <>
       <div>
